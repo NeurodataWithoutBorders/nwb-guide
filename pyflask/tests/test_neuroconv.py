91c9dccec75b0036b95caa19c5ecaeac1e928888
Garrett <gflynn@usc.edu>
2023-03-07 09:36:40 -0800

    Added test files and configurations for the base neuroconv endpoint

---
+++ pyflask/tests/test_neuroconv.py
@@ -0,0 +1,12 @@
+# --------------------- Helper Functions ---------------------
+
+# --------------------- Tests ---------------------
+# Accesses the dictionary of all interfaces and their metadata
+def test_get_all_interfaces(client):
+# def test_get_all_interfaces(client, all_interfaces):
+    all_interfaces = client.get("/neuroconv", follow_redirects=True).json
+    for key, value in all_interfaces.items():
+        assert isinstance(key, str)
+        assert isinstance(value['modality'], str)
+        assert isinstance(value['name'], str)
+        assert isinstance(value['technique'], str)
