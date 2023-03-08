f1565137473e738acd4968d54e9d8d243fbe2762
Garrett <gflynn@usc.edu>
2023-03-08 12:18:05 -0800

    Add test for combining interfaces for a schema request

--- pyflask/tests/test_neuroconv.py
+++ pyflask/tests/test_neuroconv.py
@@ -1,4 +1,18 @@
 # --------------------- Helper Functions ---------------------
+def check_schema(data):
+    assert isinstance(data, dict)
+    assert isinstance(data['required'], list)
+    assert isinstance(data['properties'], dict)
+
+
+def check_converter(data, interfaces):
+    check_schema(data)
+    for interface in interfaces:
+        assert isinstance(data['properties'][interface], dict)
+        check_schema(data)
+    assert isinstance(data['title'], str)
+    assert isinstance(data['description'], str)
+    assert isinstance(data['version'], str)
 
 # --------------------- Tests ---------------------
 # Accesses the dictionary of all interfaces and their metadata
@@ -10,3 +24,9 @@ def test_get_all_interfaces(client):
         assert isinstance(value["modality"], str)
         assert isinstance(value["name"], str)
         assert isinstance(value["technique"], str)
+
+# Uses the NWBConverter Class to combine multiple interfaces
+def test_multiple_schema_request(client):
+    interfaces = ["SpikeGLXRecordingInterface", "PhySortingInterface"][:1]
+    response = client.get(f"/neuroconv/schema?interfaces={','.join(interfaces)}", follow_redirects=True)
+    check_converter(response.json, interfaces)
