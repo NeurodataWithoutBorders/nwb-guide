e4dee158213bd5f65743e348fe2b5efa89c1064a
Garrett <gflynn@usc.edu>
2023-03-08 12:11:33 -0800

    Added test for new endpoint

--- pyflask/tests/test_neuroconv.py
+++ pyflask/tests/test_neuroconv.py
@@ -10,3 +10,11 @@ def test_get_all_interfaces(client):
         assert isinstance(value["modality"], str)
         assert isinstance(value["name"], str)
         assert isinstance(value["technique"], str)
+
+# Accesses the schema for a single interface directly
+def test_single_schema_request(client):
+    response = client.get("/neuroconv/schema/PhySortingInterface", follow_redirects=True)
+    data = response.json
+    assert isinstance(data, dict)
+    assert isinstance(data['required'], list)
+    assert isinstance(data['properties'], dict)
