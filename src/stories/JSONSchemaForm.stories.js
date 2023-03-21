8c1c13f7f23fa6865ad7b26fc70470122ee96733
Garrett <gflynn@usc.edu>
2023-03-21 12:03:28 -0700

    Add JSONSchemaForm to Storybook

---
+++ src/stories/JSONSchemaForm.stories.js
@@ -0,0 +1,34 @@
+import { JSONSchemaForm } from './JSONSchemaForm';
+
+export default {
+  title: 'Example/JSON Schema Form'
+};
+
+const Template = (args) => new JSONSchemaForm(args);
+
+const defaultSchema = {
+  title: 'Test Title',
+  description: 'This is a test description',
+  properties: {
+    test: {
+      type: 'string',
+      default: true
+    },
+    optional: {
+      type: 'string',
+      format: 'file',
+    }
+  },
+  required: ['test']
+}
+
+export const Nested = Template.bind({});
+Nested.args = {
+  schema: {
+    title: 'Nested',
+    properties: {
+      nested: defaultSchema
+    }
+  }
+};
+
