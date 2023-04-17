139743233f8b99aa02132063715a4252f63e7408
Garrett <gflynn@usc.edu>
2023-04-17 11:40:34 -0700

    Fix conversion behavior which requires interfaces to be passed

--- src/stories/pages/guided-mode/options/GuidedStubPreview.js
+++ src/stories/pages/guided-mode/options/GuidedStubPreview.js
@@ -23,7 +23,8 @@ export class GuidedStubPreviewPage extends Page {
 
         // Override with the lastest source data and metadata information
         source_data: this.info.globalState.source.results,
-        metadata: this.info.globalState.metadata.results
+        metadata: this.info.globalState.metadata.results,
+        interfaces: this.info.globalState.source.interfaces
       })
 
       .catch(e => this.notify(e.message, 'error'))
