83f3694cb8540e05564bd4c3e94ba321533b6524
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2023-04-26 14:26:59 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,8 @@ import sys
 import inspect
 from pathlib import Path
 
+from conf_extlinks import extlinks, intersphinx_mapping
+
 sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
 
 project = "NWBGuide"
