6fe9a06bc396770785fa16f4743cab8abe8a4e4c
Garrett <gflynn@usc.edu>
2023-08-18 09:25:29 -0700

    Try catching cumulative errors

--- tests/testPyinstallerExecutable.js
+++ tests/testPyinstallerExecutable.js
@@ -14,26 +14,38 @@ handleProcess(proc2, "spawn");
 
 let now = Date.now();
 
+let outputCollection = '';
+
+const regex = /.+Error: .+/
+
 function handleProcess(proc, id = "process") {
     if (proc != null) {
+
         // Listen for errors from Python process
         proc.stderr.on("data", function (data) {
-            console.error(`[${id}]: ${data}`);
+            const message = data.toString()
+            console.error(`[${id}] Error: ${data}`);
+            outputCollection+=message
+            if (regex.test(message)) throw new Error(outputCollection);
         });
 
         proc.stdout.on("data", function (data) {
-            console.log(`Time to Start: ${(Date.now() - now).toFixed(2)}ms`);
-            process.exit();
+                console.log(`Time to Start: ${(Date.now() - now).toFixed(2)}ms`);
+                process.exit( );
         });
 
-        proc.on("error", (error) => {
-            console.error(`[${id}] error: ${error.message}`);
+        const error = () => () =>  {
             throw new Error("The distributable pyflask failed to run!");
+        }
+
+        proc.on("error", (error) => {
+            console.error(`[${id}] Error: ${error.message}`);
+            error();
         });
 
         proc.on("close", (code) => {
-            console.error(`[${id}] exit: ${code}`);
-            throw new Error("The distributable pyflask failed to run!");
+            console.error(`[${id}] Exit: ${code}`);
+            error();
         });
     } else console.error("child process failed to start on port" + port);
 }
