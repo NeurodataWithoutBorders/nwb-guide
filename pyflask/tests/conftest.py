cf1873b67488c818e77a01c591a3cb593eedd084
Garrett <gflynn@usc.edu>
2023-08-21 16:18:23 -0700

    Update conftest.py

--- pyflask/tests/conftest.py
+++ pyflask/tests/conftest.py
@@ -8,15 +8,14 @@ import multiprocessing as mp
 import os
 import time
 
+PORT = 1234
 executable_flag = "--executable"
 use_executable = executable_flag in sys.argv
 is_windows = os.name == "nt"
 
-args = list(sys.argv)
-
-PORT = 1234
-exe_process = None
-
+exe_path = Path("build/flask/nwb-guide", "nwb-guide.exe" if is_windows else "nwb-guide")
+def runExecutable(port):
+    subprocess.run([exe_path, str(port)])
 
 def pytest_addoption(parser):
     parser.addoption("--executable", action="store_true", help="Run the executable instead of the standard Flask app")
@@ -24,13 +23,10 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope="session")
 def client(request):
-    exe_path = Path("build/flask/nwb-guide", "nwb-guide.exe" if is_windows else "nwb-guide")
     if use_executable and exe_path.is_file():
         if not is_windows:
             mp.set_start_method("fork")
 
-        def runExecutable(port):
-            subprocess.run([exe_path, str(port)])
 
         exe_process = Process(target=runExecutable, args=(PORT,))
         exe_process.start()
