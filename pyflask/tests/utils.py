1506dd71139cb5f3ba7774ea98064c6727d088b2
Garrett <gflynn@usc.edu>
2023-08-22 10:25:29 -0700

    Raise status errors

--- pyflask/tests/utils.py
+++ pyflask/tests/utils.py
@@ -2,19 +2,21 @@ import requests
 
 
 def get(path, client):
-    return (
-        (requests.get(f"http://localhost:{client}/{path}", allow_redirects=True)).json()
-        if isinstance(client, int)
-        else client.get(f"/{path}", follow_redirects=True).json
-    )
+    if isinstance(client,int):
+        r = (requests.get(f"http://localhost:{client}/{path}", allow_redirects=True))
+        r.raise_for_status()
+        return r.json()
+    else:
+        return client.get(f"/{path}", follow_redirects=True).json
 
 
 def post(path, json, client):
-    return (
-        (requests.post(f"http://localhost:{client}/{path}", json=json, allow_redirects=True)).json()
-        if isinstance(client, int)
-        else client.post(path, json=json, follow_redirects=True).json
-    )
+    if isinstance(client,int):
+        r = requests.post(f"http://localhost:{client}/{path}", json=json, allow_redirects=True)
+        r.raise_for_status()
+        return r.json()
+    else:
+        return client.post(path, json=json, follow_redirects=True).json
 
 
 def get_converter_output_schema(interfaces: dict):
