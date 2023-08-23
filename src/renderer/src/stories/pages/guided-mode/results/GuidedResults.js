566e7e01a58aecef8b28e428cd25ad2565598958
Garrett <gflynn@usc.edu>
2023-08-23 14:59:06 -0700

    Fix results page

--- src/renderer/src/stories/pages/guided-mode/results/GuidedResults.js
+++ src/renderer/src/stories/pages/guided-mode/results/GuidedResults.js
@@ -93,7 +93,7 @@ export class GuidedResultsPage extends Page {
                     <hr />
                     <ol>
                         ${Object.values(results)
-                            .flat((v) => Object.values(v))
+                            .map((v) => Object.values(v)).flat()
                             .map((o) => html`<li>${o.file}</li>`)}
                     </ol>
                 </div>
