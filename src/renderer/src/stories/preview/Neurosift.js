bbb1cebc790d3fe2a6afa2c01cecaa015e6b3c6c
Garrett <gflynn@usc.edu>
2023-08-23 16:07:21 -0700

    Show loader for iframe

--- src/renderer/src/stories/preview/Neurosift.js
+++ src/renderer/src/stories/preview/Neurosift.js
@@ -1,6 +1,8 @@
 import { LitElement, css, html } from "lit";
 import { baseUrl } from "../../globals";
 
+import { Loader } from '../Loader'
+
 export function getURLFromFilePath(file, projectName) {
     const regexp = new RegExp(`.+(${projectName}.+)`);
     return `${baseUrl}/stubs/${file.match(regexp)[1]}`;
@@ -9,9 +11,36 @@ export function getURLFromFilePath(file, projectName) {
 export class Neurosift extends LitElement {
     static get styles() {
         return css`
-            iframe {
+
+            :host {
                 width: 100%;
                 height: 100%;
+                display: grid;
+                grid-template-rows: 100%;
+                grid-template-columns: 100%;
+                --loader-color: hsl(200, 80%, 50%);
+            }
+
+            :host > * {
+                width: 100%;
+                height: 100%;
+            }
+
+            div {
+                display: flex;
+                align-items: center; 
+                justify-content: center;
+            }
+
+            span {
+                font-size: 14px;
+            }
+
+            small {
+                padding-left: 10px;
+            }
+
+            iframe {
                 border: 0;
             }
         `;
@@ -22,10 +51,26 @@ export class Neurosift extends LitElement {
         this.url = url;
     }
 
+
     render() {
-        return html`<iframe
+
+        return html`
+        <div>
+            <div>
+                ${new Loader()}
+                <small>Loading Neurosift view...</small>
+            </div>
+        </div>
+        <iframe
+            hidden
             class="iframe-placeholder"
             src="https://flatironinstitute.github.io/neurosift/?p=/nwb&url=${this.url}"
+            @load=${function (ev) {
+                const sibling = ev.target.previousSibling.previousSibling
+                console.log(sibling)
+                sibling.remove()
+                ev.target.removeAttribute('hidden')
+            }}
         ></iframe>`;
     }
 }
