c8905f7e308d9920383083fd4ff6823a21ad21cf
Garrett Michael Flynn <46533749+GarrettMFlynn@users.noreply.github.com>
2023-08-24 10:06:50 -0700

    Update main.ts

--- src/main/main.ts
+++ src/main/main.ts
@@ -265,7 +265,6 @@ function initialize() {
   }
 
   const quit_app = () => {
-    console.error('APP QUITTING')
     globals.mainWindow.close();
     if (!globals.mainWindow.closed) globals.mainWindow.destroy()
   };
