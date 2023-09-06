1ca85232b4ec9c61e102849b5e31c5a84d6ae381
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2023-09-06 14:13:04 -0400

    Update test_startup.py

--- pyflask/tests/test_startup.py
+++ pyflask/tests/test_startup.py
@@ -3,5 +3,5 @@ from utils import get, post, get_converter_output_schema
 
 def test_preload_imports(client):
     """Verify that the preload import endpoint returned good status."""
-    result = get("startup", client)
+    result = get("startup/preload-imports", client)
     assert result == True
