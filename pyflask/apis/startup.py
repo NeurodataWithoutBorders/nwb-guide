c78eff6910e3c8296931ae1d15fc21d23f9e8b7a
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2023-09-06 13:31:57 -0400

    Update startup.py

--- pyflask/apis/startup.py
+++ pyflask/apis/startup.py
@@ -1,6 +1,8 @@
 """API endpoint definitions for startup operations."""
 from flask_restx import Namespace, Resource
 
+from errorHandlers import notBadRequestException
+
 startup_api = Namespace("startup", description="API for startup commands related to the NWB GUIDE.")
 
 parser = startup_api.parser()
@@ -19,3 +21,23 @@ class Echo(Resource):
     def get(self):
         args = parser.parse_args()
         return args["arg"]
+
+@startup_api.route("/preload-imports")
+class PreloadImports(Resource):
+    """
+    Preload various imports on startup instead of waiting for them later on.
+
+    Python caches all modules that have been imported at least once in the same kernel,
+    even if their namespace is not always exposed to a given scope. This means that later imports
+    simply expose the cached namespaces to their scope instead of retriggering the entire import.
+    """
+    @startup_api.doc(responses={200: "Success", 400: "Bad Request", 500: "Internal server error"})
+    def get(self):
+        try:
+            import neuroconv
+
+            return True
+        except Exception as exception:
+            if notBadRequestException(exception=exception):
+                startup_api.abort(500, str(exception))
+            raise exception
