a9ddea5f8f250dbff3208fdfaaa59673e3f1e436
Cody Baker <cbaker9@nd.edu>
2023-09-13 23:48:18 -0400

    simplify python

--- prepare_pyinstaller_spec.py
+++ prepare_pyinstaller_spec.py
@@ -3,28 +3,21 @@ Calling `pyi-makespec` regenerates the base spec file, but we need to extend the
 
 This script is run automatically as a part of `npm run build:flask:spec` after the `pyi-makespec` call.
 """
+from pathlib import Path
 
+with open(file=Path(__file__).parent / "nwb-guide.spec", mode="r") as io:
+    lines = io.readlines()
 
-def test():
-    from pathlib import Path
+lines.insert(1, "import sys\n")
+lines.insert(2, "sys.setrecursionlimit(sys.getrecursionlimit() * 5)\n")
+lines.insert(3, "\n")
 
-    with open(file=Path(__file__).parent / "nwb-guide.spec", mode="r") as io:
-        lines = io.readlines()
+# Originally this was a separate `npm` command per platform to account for CLI syntax differences between ; and :
+# The spec file is, however, the same across platforms
+data_line_index = lines.index("datas = []\n")
+lines[data_line_index] = "datas = [('./paths.config.json', '.'), ('./package.json', '.')]\n"
 
-    lines.insert(1, "import sys\n")
-    lines.insert(2, "sys.setrecursionlimit(sys.getrecursionlimit() * 5)\n")
-    lines.insert(3, "\n")
+with open(file=Path(__file__).parent / "nwb-guide.spec", mode="w") as io:
+    io.writelines(lines)
 
-    # Originally this was a separate `npm` command per platform to account for CLI syntax differences between ; and :
-    # The spec file is, however, the same across platforms
-    data_line_index = lines.index("datas = []\n")
-    lines[data_line_index] = "datas = [('./paths.config.json', '.'), ('./package.json', '.')]\n"
-
-    with open(file=Path(__file__).parent / "nwb-guide.spec", mode="w") as io:
-        io.writelines(lines)
-
-    print("Sucessfully injected recursion depth extension and json paths!")
-
-
-if __name__ == "__main__":
-    test()
+print("Sucessfully injected recursion depth extension and json paths!")
