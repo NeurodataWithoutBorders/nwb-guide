67e2ad878794275435dfd632f60fd190c3571137
CodyCBakerPhD <codycbakerphd@gmail.com>
2023-11-28 00:02:34 -0500

    remove comments

--- pyflask/tests/test_generate_tutorial_data.py
+++ pyflask/tests/test_generate_tutorial_data.py
@@ -6,10 +6,7 @@ def test_generate_tutorial_data(client, tmp_path: Path):
     # assert client is None
     result = client.post(
         path="/tutorial/generate",
-        # path=f"/tutorial/generate/base_path={tmp_path}",
-        # json=dict(),
         json=dict(base_path=str(tmp_path)),
-        # client=client,
     )
     assert result is None
     assert len(list(Path(tmp_path).iterdir())) != 0
