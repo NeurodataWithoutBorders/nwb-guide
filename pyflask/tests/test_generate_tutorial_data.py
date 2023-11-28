f2902ccd51a0ebce19582896bbb2563e26e1d1c2
CodyCBakerPhD <codycbakerphd@gmail.com>
2023-11-27 22:55:58 -0500

    fix

--- pyflask/tests/test_generate_tutorial_data.py
+++ pyflask/tests/test_generate_tutorial_data.py
@@ -3,6 +3,6 @@ from pathlib import Path
 
 
 def test_generate_tutorial_data(tmp_path, client):
-    post(f"tutorial/generate/{tmp_path}", tmp_path, client)
+    post(f"tutorial/generate/{tmp_path}", client)
     assert (Path(tmp_path) / "spikeglx").exists()
     assert (Path(tmp_path) / "phy").exists()
