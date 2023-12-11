17e788af7cfa9438e0fa8f5d8f0ccd6f518a2b23
Garrett Michael Flynn <garrettmflynn@gmail.com>
2023-12-11 13:21:53 -0800

    Enable embargo

--- schemas/dandi-create.schema.ts
+++ schemas/dandi-create.schema.ts
@@ -1,6 +1,4 @@
 import create from './json/dandi/create.json' assert { type: "json" }
 const schema = structuredClone(create)
-delete schema.properties.embargo_status
-delete schema.properties.nih_award_number
 
 export default schema
