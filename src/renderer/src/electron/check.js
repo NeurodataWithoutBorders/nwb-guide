3b44bc46abdb535ef1ea94d575136caec1bda3bb
Garrett Michael Flynn <garrettmflynn@gmail.com>
2023-12-14 10:56:13 -0800

    Add storybook polyfill and add missing dependency

--- src/renderer/src/electron/check.js
+++ src/renderer/src/electron/check.js
@@ -1,3 +1,14 @@
 // var userAgent = navigator.userAgent.toLowerCase();
 // export const isElectron = userAgent.indexOf(" electron/") > -1;
+
+// Storybook polyfill
+if (!globalThis.commoners && window.location.href.includes("iframe.html")) {
+    globalThis.commoners = {
+        version: 'Storybook',
+        target: 'web',
+        plugins: {},
+        services: {}
+    }
+}
+
 export default commoners.target === "desktop";
