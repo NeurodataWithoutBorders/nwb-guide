b3e2318422dc7e1abb93979a02492dc5e21be279
Garrett Michael Flynn <garrettmflynn@gmail.com>
2024-01-29 11:24:34 -0800

    Remove regenerated pipeline files (#572)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>

--- src/renderer/src/stories/pages/settings/SettingsPage.js
+++ src/renderer/src/stories/pages/settings/SettingsPage.js
@@ -9,7 +9,7 @@ import developerGlobalSchema from "../../../../../../schemas/json/developer/glob
 import { validateDANDIApiKey } from "../../../validation/dandi";
 
 import { Button } from "../../Button.js";
-import { global, save } from "../../../progress/index.js";
+import { global, remove, save } from "../../../progress/index.js";
 import { merge, setUndefinedIfNotDeclared } from "../utils.js";
 
 import { notyf } from "../../../dependencies/globals.js";
@@ -34,11 +34,15 @@ function saveNewPipelineFromYaml(name, sourceData, rootFolder) {
         });
     });
 
+    const updatedName = header(name);
+
+    remove(updatedName, true);
+
     save({
         info: {
             globalState: {
                 project: {
-                    name: header(name),
+                    name: updatedName,
                     initialized: true,
                 },
 
