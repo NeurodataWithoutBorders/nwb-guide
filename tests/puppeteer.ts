448173a822454da7dfa8713fa69eb40714258ca1
Garrett Michael Flynn <garrettmflynn@gmail.com>
2024-01-31 10:24:09 -0800

    Add messages during test

--- tests/puppeteer.ts
+++ tests/puppeteer.ts
@@ -12,12 +12,14 @@ export const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
 
 
 const beforeStart = async (startupTime) => {
-  const process = spawn('npm', ['run', 'start']) // Run Start Script from package.json
 
+  console.log('Spawning Electron application')
+  const process = spawn('npm', ['run', 'start']) // Run Start Script from package.json
   process.stdout.on('data', (data) =>  console.log(`[electron] ${data}`));
   process.stderr.on('data', (data) => console.error(`[electron] ${data}`));
   process.on('close', (code) => console.log(`[electron] Exited with code ${code}`));
-
+  
+  console.log('Waiting for the application to open...')
   await sleep(startupTime) // Wait for five seconds for Electron to open
 }
 
@@ -44,6 +46,9 @@ export const connect = () => {
         mockExit.mockRestore()
     });
 
+
+    console.log('Deriving Electron Connection Details')
+
     const browserURL = `http://localhost:${electronDebugPort}`
     const browser = output.browser = await puppeteer.launch({ headless: 'new' })
     const page = output.page = await browser.newPage();
@@ -54,11 +59,17 @@ export const connect = () => {
     delete output.page
 
     // Connect to browser WS Endpoint
+    console.log('Connecting to Electron Instance via Websockets')
     const browserWSEndpoint = endpoint.replace('localhost', '0.0.0.0')
     output.browser = await puppeteer.connect({ browserWSEndpoint, defaultViewport: null })
+
+    console.log('Grabbing the first Electron page')
     const pages = await output.browser.pages()
     output.page = pages[0]
-  }, sleepBeforeQuery + 1000)
+
+    console.log('Successfully connected to Electron!')
+
+  }, sleepBeforeQuery + 5000)
 
   afterAll(async () => {
     if (output.browser) await output.browser.close() // Will also exit the Electron instance
