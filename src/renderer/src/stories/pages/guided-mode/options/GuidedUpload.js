823c0a32642cf48b835ec49e9a0a82b2b1e21687
Garrett Michael Flynn <garrettmflynn@gmail.com>
2024-02-06 15:25:10 -0800

    Update next text

--- src/renderer/src/stories/pages/guided-mode/options/GuidedUpload.js
+++ src/renderer/src/stories/pages/guided-mode/options/GuidedUpload.js
@@ -88,7 +88,6 @@ export class GuidedUploadPage extends Page {
     }
 
     footer = {
-        next: "Upload Project",
         onNext: async () => {
             await this.save(); // Save in case the conversion fails
 
