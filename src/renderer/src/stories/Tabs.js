3b664b5d216b1dc1f8193bec3c5e07d35c1911e6
Garrett Michael Flynn <garrettmflynn@gmail.com>
2024-02-21 13:44:55 -0600

    Design tab component

---
+++ src/renderer/src/stories/Tabs.js
@@ -0,0 +1,175 @@
+import { LitElement, css, html } from "lit";
+import {
+    errorHue,
+    warningHue,
+} from "./globals";
+
+const faSize = "1em";
+const faColor = "#000000";
+
+export class Tabs extends LitElement {
+    static get styles() {
+        return css`
+            * {
+                box-sizing: border-box;
+            }
+
+            :host {
+                display: block;
+            }
+
+            #tab-toggles {
+                display: flex;
+                align-items: center;
+                overflow-x: auto;
+            }
+
+            .toggle {
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+                padding: 15px 20px;
+                cursor: pointer;
+                user-select: none;
+                background-color: rgb(235, 235, 235);
+                flex-grow: 1;
+                min-width: 200px;
+                width: fit-content;
+                border-left: 2px solid gray;
+            }
+
+            .toggle:first-child {
+                border-left: none;
+            }
+
+            .status {
+                width: 25px;
+                height: 25px;
+                border-radius: 50%;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                font-size: 12px;
+            }
+
+            .statuses {
+                display: flex;
+                gap: 5px;
+            }
+
+            .status.errors {
+                background-color: hsl(${errorHue}, 100%, 70%);
+                color: white;
+            }
+
+            .status.warnings {
+                background-color: hsl(${warningHue}, 100%, 70%);
+                color: black;
+            }
+
+            .toggle:hover {
+                background-color: gainsboro;
+            }
+
+            .toggle.active {
+                background-color: gainsboro;
+            }
+
+            .toggle .name {
+                font-size: 110%;
+                font-weight: bold;
+            }
+
+            .toggle .subtitle {
+                font-size: 90%;
+            }
+
+            .content {
+                display: none;
+            }
+        `;
+    }
+
+    static get properties() {
+        return {
+            items: { type: Array },
+            // name: { type: String, reflect: true },
+            // open: { type: Boolean, reflect: true },
+            // disabled: { type: Boolean, reflect: true },
+            // status: { type: String, reflect: true },
+        };
+    }
+
+    constructor({
+        items = [],
+        selected = 0,
+        contentPadding = "15px",
+    } = {}) {
+        super();
+        this.items= items
+        this.selected = selected
+        this.contentPadding = contentPadding
+    }
+
+    updated() {
+        this.toggle(this.selected)
+    }
+
+    toggle(index) {
+        const toggles = this.shadowRoot.getElementById("tab-toggles").children;
+        toggles[index].click()
+    }
+
+    render() {
+
+
+        return html`
+            <div id="tab-toggles">
+            ${this.items.map((item, i) => {
+                return html`
+                    <div class="toggle" 
+                    
+                        @click=${() => {
+
+                            Array.from(this.shadowRoot.getElementById("tab-toggles").children).forEach((toggle, j) => {
+                                toggle.classList.toggle("active", i === j)
+                            })
+
+                            Array.from(this.shadowRoot.getElementById("tab-content").children).forEach((content, j) => {
+                                content.style.display = i === j ? 'block' : ""
+                            })
+                        }}
+                    >
+                        <div>
+                            <span class="name">${item.name}</span><br>
+                            <span class="subtitle">${item.subtitle}</span>
+                        </div>
+                        <div class="statuses">
+                            ${Object.entries(item.status).map(([status, value]) => {
+                                if (!value) return
+                                return html`
+                                    <span class="status ${status}">${value}</span>
+                                `;
+                            })}
+                        </div>
+                    </div>
+                `;
+            })}
+            </div>
+            <div id="tab-content">
+                ${this.items.map((item) => {
+                    return html`
+                        <div 
+                            class="content" 
+                            style="padding: ${this.contentPadding}"
+                        >
+                            ${item.content}
+                        </div>
+                    `;
+                })}
+            </div>
+        `
+    }
+}
+
+customElements.get("nwb-tabs") || customElements.define("nwb-tabs", Tabs);
---
+++ src/renderer/src/stories/Tabs.stories.js
@@ -0,0 +1,73 @@
+import { Tabs } from "./Tabs";
+
+export default {
+    title: "Components/Tabs",
+};
+
+const Template = (args) => new Tabs(args);
+
+const base = [
+    {
+        name: "NWBFile",
+        status: {
+            errors: 0,
+            warnings: 0,
+        },
+        content: "NWBFile information",
+        subtitle: "NWB File Properties",
+    },
+    {
+        name: "Subject",
+        status: {
+            errors: 3,
+            warnings: 0,
+        },
+        content: "Subject information",
+        subtitle: "Subject Properties",
+    },
+    {
+        name: "Ecephys",
+        status: {
+            errors: 0,
+            warnings: 3,
+        },
+        content: "Ecephys information",
+        subtitle: "Ecephys Properties",
+    },
+    {
+        name: "Ophys",
+        content: "Ophys information",
+        subtitle: "Ophys Properties",
+        status: {
+            errors: 5,
+            warnings: 3,
+        },
+    },
+]
+
+export const Basic = Template.bind({});
+Basic.args = {
+    items: base,
+    selected: 1
+};
+
+// export const Valid = Template.bind({});
+// Valid.args = {
+//     ...base,
+//     status: "valid",
+//     content: "WOOHOO!",
+// };
+
+// export const Warning = Template.bind({});
+// Warning.args = {
+//     ...base,
+//     status: "warning",
+//     content: "OOO",
+// };
+
+// export const Error = Template.bind({});
+// Error.args = {
+//     ...base,
+//     status: "error",
+//     content: "BOO",
+// };
