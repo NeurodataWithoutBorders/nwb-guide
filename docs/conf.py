2fa6f1baa27d8012149781cc52085fed623e0394
Ben Dichter <ben.dichter@gmail.com>
2024-03-10 17:51:00 -0400

    Update docs/conf.py
    Co-authored-by: Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -71,7 +71,7 @@ html_theme_options = {
     "icon_links": [
         {
             "name": "GitHub",
-            "url": "https://github.com/catalystneuro/neuroconv",
+            "url": "https://github.com/NeurodataWithoutBorders/nwb-guide",
             "icon": "fa-brands fa-github",
             "type": "fontawesome",
         },
