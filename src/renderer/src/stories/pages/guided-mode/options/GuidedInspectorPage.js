dd77515ef0b6761f09d069f4f94be987c5c69e6a
Garrett Michael Flynn <garrettmflynn@gmail.com>
2024-03-14 16:06:59 -0700

    Update Legend to Render Multi-Session View Well

--- src/renderer/src/stories/pages/guided-mode/options/GuidedInspectorPage.js
+++ src/renderer/src/stories/pages/guided-mode/options/GuidedInspectorPage.js
@@ -39,9 +39,9 @@ export class GuidedInspectorPage extends Page {
         this.style.height = "100%"; // Fix main section
 
         Object.assign(this.style, {
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
+            display: "grid",
+            gridTemplateRows: "calc(100% - 120px) 1fr",
+            rowGap: "10px"
         });
     }
 
@@ -136,6 +136,7 @@ export class GuidedInspectorPage extends Page {
                         const items = this.report.messages;
 
                         const list = new InspectorList({ items, emptyMessage });
+
                         Object.assign(list.style, {
                             height: "100%",
                         });
