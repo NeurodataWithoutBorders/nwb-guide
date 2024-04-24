30f91a65efca79d98f589802ed7b9fdffb7a7445
rly <rly@lbl.gov>
2024-04-24 13:09:08 -0700

    Undo reset title

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,6 @@ templates_path = ["_templates"]
 master_doc = "index"
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_theme = "pydata_sphinx_theme"
-html_title = "NWB GUIDE"
 html_static_path = ["_static"]
 
 html_show_sourcelink = False
