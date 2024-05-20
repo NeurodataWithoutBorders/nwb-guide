c98f4ed5fcc18ad7aefbec863c04a5bab3973673
Garrett Michael Flynn <garrettmflynn@gmail.com>
2024-05-20 15:44:39 -0700

    Ensure users are notified about possible data that the GUIDE misses

--- src/renderer/src/stories/pages/guided-mode/results/GuidedResults.js
+++ src/renderer/src/stories/pages/guided-mode/results/GuidedResults.js
@@ -7,6 +7,11 @@ import { getStubArray } from "../options/GuidedStubPreview.js";
 import { getSharedPath } from "../../../preview/NWBFilePreview.js";
 
 import { electron, path } from "../../../../electron/index.js";
+
+import manualActionsJSON from '../../../../../../../manualActions.json';
+
+import { CodeBlock } from "../../../CodeBlock.js";
+
 const { ipcRenderer } = electron;
 
 export class GuidedResultsPage extends Page {
@@ -43,6 +48,8 @@ export class GuidedResultsPage extends Page {
         if (!conversion)
             return html`<div style="text-align: center;"><p>Your conversion failed. Please try again.</p></div>`;
 
+
+        // Show a snippet for how to open the NWB file
         return html`
             <p>Your data was successfully converted to NWB!</p>
             <ol style="margin: 10px 0px; padding-top: 0;">
@@ -51,6 +58,38 @@ export class GuidedResultsPage extends Page {
                     .sort()
                     .map((id) => html`<li>${id}</li>`)}
             </ol>
+            <h4>But what about my other data?</h4>
+            <p>The GUIDE still can't do everything. You may need to manually adjust the NWB file to ensure it contains all the necessary data.</p>
+            ${new CodeBlock({
+                text: `from pynwb import NWBHDF5IO, NWBFile, TimeSeries
+
+filename = "${this.#sharedPath()}"
+
+# Open the file
+with NWBHDF5IO(filename, "r+") as io:
+    nwbfile = io.read()
+
+    # Adjust the file as needed
+    # ...
+
+    # Write the modified file
+    io.write(nwbfile)
+`,
+
+            })}
+            <h5>Related Documentation</h5>
+            <div style="display: flex; gap: 10px; margin-top: 15px; padding-bottom: 5px; margin-bottom: 10px; overflow: auto;">
+            ${
+                manualActionsJSON.map(({ name, description, url }) => html`
+                    <div style="min-width: 300px; padding: 20px; background-color: #f0f0f0; border-radius: 5px;">
+                        <h4 style="margin-bottom: 5px;"><a href=${url} target="_blank" style="text-decoration: none;">${name}</a></h4>
+                        <small>${description}</small>
+                    </div>
+                `)
+            }
+            </div>
+            <p>For more information, please refer to the <a href="https://pynwb.readthedocs.io/en/stable/" target="_blank">PyNWB</a> and <a href="https://neurodatawithoutborders.github.io/matnwb/" target="_blank">MatNWB</a> documentation.</p>
+            
         `;
     }
 }
