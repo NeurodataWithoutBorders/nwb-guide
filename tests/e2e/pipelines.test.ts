3234e760168992b1e741f5604d2a4487615557fe
Garrett Michael Flynn <garrettmflynn@gmail.com>
2024-05-31 12:42:22 -0700

    Update pipelines.test.ts

--- tests/e2e/pipelines.test.ts
+++ tests/e2e/pipelines.test.ts
@@ -24,121 +24,133 @@ beforeAll(() => initTests({ screenshots: false, data: false }))
 
 describe('Run example pipelines', () => {
 
-    test('Ensure test data is present', () => {
-      expect(existsSync(testGINPath)).toBe(true)
-    })
+  test('Ensure test data is present', () => {
+    expect(existsSync(testGINPath)).toBe(true)
+  })
 
 
-    test('Ensure number of example pipelines starts at zero', async () => {
-      const nPipelines = await evaluate(() => document.getElementById('guided-div-resume-progress-cards').children.length)
-      expect(nPipelines).toBe(0)
-    })
+  test('Ensure number of example pipelines starts at zero', async () => {
+    const nPipelines = await evaluate(() => document.getElementById('guided-div-resume-progress-cards').children.length)
+    expect(nPipelines).toBe(0)
+  })
 
-    // NOTE: The following code is dependent on the presence of test data on the user's computer
-    pipelineDescribeFn('Generate and run pipeline from YAML file', () => {
+  // NOTE: The following code is dependent on the presence of test data on the user's computer
+  pipelineDescribeFn('Generate and run pipeline from YAML file', () => {
 
-      test('All example pipelines are created', async ( ) => {
+    test('All example pipelines are created', async () => {
 
-        const result = await evaluate(async (testGINPath) => {
+      const result = await evaluate(async (testGINPath) => {
 
-            // Transition to settings page
-            const dashboard = document.querySelector('nwb-dashboard')
-            dashboard.sidebar.select('settings')
-            await new Promise(resolve => setTimeout(resolve, 200))
+        // Transition to settings page
+        const dashboard = document.querySelector('nwb-dashboard')
+        dashboard.sidebar.select('settings')
+        await new Promise(resolve => setTimeout(resolve, 200))
 
-            const page = dashboard.page
+        const page = dashboard.page
 
-            // Open relevant accordion
-            const accordion = page.form.accordions['developer']
-            accordion.toggle(true)
+        // Open relevant accordion
+        const accordion = page.form.accordions['developer']
+        accordion.toggle(true)
 
-            // Generate example pipelines
-            const folderInput = page.form.getFormElement(["developer", "testing_data_folder"])
-            folderInput.updateData(testGINPath)
+        // Generate example pipelines
+        const folderInput = page.form.getFormElement(["developer", "testing_data_folder"])
+        folderInput.updateData(testGINPath)
 
-            const button = folderInput.nextSibling
-            const results = await button.onClick()
+        const button = folderInput.nextSibling
+        const results = await button.onClick()
 
-            page.save()
-            page.dismiss() // Dismiss any notifications
+        page.save()
+        page.dismiss() // Dismiss any notifications
 
-            return results
+        return results
 
-        }, testGINPath)
+      }, testGINPath)
 
-        await sleep(500) // Wait for notification to dismiss
+      await sleep(500) // Wait for notification to dismiss
 
-        const allPipelineNames = Object.keys(examplePipelines).reverse()
+      const allPipelineNames = Object.keys(examplePipelines).reverse()
 
-        expect(result).toEqual(allPipelineNames.map(name => { return {name, success: true} }))
+      expect(result).toEqual(allPipelineNames.map(name => { return { name, success: true } }))
 
-        await takeScreenshot(join('test-pipelines', 'created'))
-
-
-        // Transition back to the conversions page and count pipelines
-        const renderedPipelineNames = await evaluate(async () => {
-          const dashboard = document.querySelector('nwb-dashboard')
-          dashboard.sidebar.select('/')
-          await new Promise(resolve => setTimeout(resolve, 200))
-          const pipelineCards = document.getElementById('guided-div-resume-progress-cards').children
-          return Array.from(pipelineCards).map(card => card.info.project.name)
-        })
-
-        await takeScreenshot(join('test-pipelines', 'list'), 500)
+      await takeScreenshot(join('test-pipelines', 'created'))
 
 
-        // Assert all the pipelines are present
-        expect(renderedPipelineNames.sort()).toEqual(allPipelineNames.map(header).sort())
-
+      // Transition back to the conversions page and count pipelines
+      const renderedPipelineNames = await evaluate(async () => {
+        const dashboard = document.querySelector('nwb-dashboard')
+        dashboard.sidebar.select('/')
+        await new Promise(resolve => setTimeout(resolve, 200))
+        const pipelineCards = document.getElementById('guided-div-resume-progress-cards').children
+        return Array.from(pipelineCards).map(card => card.info.project.name)
       })
 
+      await takeScreenshot(join('test-pipelines', 'list'), 500)
 
-      for (let pipeline in examplePipelines) {
 
-        const pipelineParsed = header(pipeline)
-        const info = examplePipelines[pipeline]
-        const describeFn = info.test === false ? describe.skip : describe
-
-        describeFn(`${pipelineParsed}`, async ( ) => {
-
-            test(`Full conversion completed`, async () => {
-              const wasFound = await evaluate(async (toMatch) => {
-                const dashboard = document.querySelector('nwb-dashboard')
-                const pipelines = document.getElementById('guided-div-resume-progress-cards').children
-                const found = Array.from(pipelines).find(card => card.info.project.name === toMatch)
-                if (found) {
-                  found.querySelector('button')!.click()
-
-                  const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
-                  const toHome = () => dashboard.page.to('/')
-
-                  // Update this with a while loop to advance through the pipeline until back at the home page
-                  while (document.querySelector('nwb-dashboard').page.info.id !== '//'){
-                    await sleep(100)
-                    try {
-                     await dashboard.next()
-                     await dashboard.page.rendered
-                     const id = dashboard.page.info.id
-                     takeScreenshot(join('test-pipelines', 'conversion', toMatch, id))
-                     if (id === '//conversion') break // Conversion page is the last page
-                    } catch (e) {
-                      await toHome()
-                      return e.message // Return error
-                    }
-                  }
-
-                  await toHome()
-
-                  return true
-                }
-              }, pipelineParsed)
-
-              expect(wasFound).toBe(true)
-            })
-
-        })
-
-      }
+      // Assert all the pipelines are present
+      expect(renderedPipelineNames.sort()).toEqual(allPipelineNames.map(header).sort())
 
     })
+
+
+    for (let pipeline in examplePipelines) {
+
+      const pipelineParsed = header(pipeline)
+      const info = examplePipelines[pipeline]
+      const describeFn = info.test === false ? describe.skip : describe
+
+      describeFn(`${pipelineParsed}`, async () => {
+
+        test(`Full conversion completed`, async () => {
+
+          // Start pipeline
+          const started = await evaluate(async (toMatch) => {
+            // const dashboard = document.querySelector('nwb-dashboard')
+            // if (dashboard.page.info.id !== '//') await dashboard.page.to('/') // Go back to home page
+            const pipelines = document.getElementById('guided-div-resume-progress-cards').children
+            const found = Array.from(pipelines).find(card => card.info.project.name === toMatch)
+            if (found) found.querySelector('button')!.click()
+            return !!found
+          }, pipelineParsed)
+
+          expect(started).toBe(true)
+
+          let pageId;
+
+          while (pageId !== '/') {
+            pageId = await evaluate(async () => {
+              const dashboard = document.querySelector('nwb-dashboard')
+
+              const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+
+              // Update this with a while loop to advance through the pipeline until back at the home page
+              await sleep(100)
+
+              try {
+                await dashboard.next()
+                await dashboard.page.rendered
+              } catch (e) { await dashboard.page.to('/') } // Go back to the home page if error
+
+              return dashboard.page.info.id
+            })
+
+            takeScreenshot(join('test-pipelines', 'conversion', pipelineParsed, pageId))
+
+            // Stop the pipeline. Conversion page is the last page
+            if (pageId === '//conversion') {
+              pageId = await evaluate(async () => {
+                const dashboard = document.querySelector('nwb-dashboard')
+                await dashboard.page.to('/')
+                return dashboard.page.info.id
+              })
+
+              expect(pageId).toBe('/') // Will break while loop if failed
+            
+            }
+          }
+
+        })
+      })
+    }
+  })
 })
