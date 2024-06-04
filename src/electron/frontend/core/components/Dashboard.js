d6a05ac5415d10cb37bff5761c566d67794d947f
Garrett Michael Flynn <garrettmflynn@gmail.com>
2024-06-04 09:55:26 -0700

    Ensure checking state properly

--- src/electron/frontend/core/components/Dashboard.js
+++ src/electron/frontend/core/components/Dashboard.js
@@ -249,12 +249,12 @@ export class Dashboard extends LitElement {
         const workflowConfig = page.workflow ?? (page.workflow = {});
         const workflowValues = page.info.globalState?.project?.workflow ?? {};
 
-        Object.entries(workflowValues).forEach(([key, state = {}]) => {
+        Object.entries(workflowValues).forEach(([key, value]) => {
             const config = workflowConfig[key] ?? (workflowConfig[key] = {});
-            const value = (config.value = workflowValues[key]);
+            config.value = value;
 
-            if (state.elements) {
-                const elements = state.elements;
+            const { elements } = config;
+            if (elements) {
                 if (value) elements.forEach((el) => el.removeAttribute("hidden"));
                 else elements.forEach((el) => el.setAttribute("hidden", true));
             }
