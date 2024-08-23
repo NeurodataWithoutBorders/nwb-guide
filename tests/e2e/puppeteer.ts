03da3e5d6261b94027f37165338309345b422548
rly <rly@lbl.gov>
2024-08-23 14:55:57 -0700

    Increase beforeStartTimeout for failing macos example data test

--- tests/e2e/puppeteer.ts
+++ tests/e2e/puppeteer.ts
@@ -22,7 +22,7 @@ const beforeStart = (timeout) => new Promise(async (resolve, reject) => {
   process.stdout.on('data', handleOutput);
   process.stderr.on('data', handleOutput);
   process.on('close', (code) => console.log(`[electron] Exited with code ${code}`));
-  await sleep(timeout) // Wait for five seconds for Electron to open
+  await sleep(timeout)
 
   reject('Failed to open Electron window successfully.')
 })
@@ -33,14 +33,12 @@ type BrowserTestOutput = {
   browser?: puppeteer.Browser,
 }
 
-const beforeStartTimeout = 60 * 1000 // Wait for 1 minute for Electron to open (mostly for Windows)
+const beforeStartTimeout = 2 * 60 * 1000 // Wait 2 minutes for Electron to open
 const protocolTimeout = 10 * 60 * 1000 // Creating the test dataset can take up to 10 minutes (mostly for Windows)
 
 export const connect = () => {
 
-
   const output: BrowserTestOutput = {}
-
 
   beforeAll(async () => {
 
@@ -52,7 +50,7 @@ export const connect = () => {
     });
 
     const browserURL = `http://localhost:${electronDebugPort}`
-    const browser = output.browser = await puppeteer.launch({ headless: 'new'})
+    const browser = output.browser = await puppeteer.launch({ headless: 'new' })
     const page = output.page = await browser.newPage();
     await page.goto(browserURL);
     const endpoint = await page.evaluate(() => fetch(`json/version`).then(res => res.json()).then(res => res.webSocketDebuggerUrl))
